4815a5ee0044a71187d26714dae554e3fceb86cf
sonephet <sonephetmnlv@gmail.com>
2026-04-28 11:27:31 +0700

    update

--- src/api/test/controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express'
-
-// Kept for backwards compatibility (old test route removed from router).
-export const uploadFileController = async (_req: Request, res: Response) => {
-  return res.status(410).json({
-    status: 'error',
-    message: 'Deprecated. Use POST /files/upload instead.'
-  })
-}
